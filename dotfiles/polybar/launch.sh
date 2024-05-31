d6626a840b0df9ed607ac6837aac8c942b1b6639
James Aylward <james.michael.aylward@gmail.com>
2024-05-31 10:00:21 +1000

    added monitor bars (not working)

---
+++ dotfiles/polybar/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+killall -q polybar
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload &
+  done
+else
+  polybar --reload &
+fi
